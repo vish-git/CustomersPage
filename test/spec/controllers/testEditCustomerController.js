6d656b3a1c9f9f21764bbdbc02cbb40873d9553e
vishwanath <vishwanathph@gmail.com>
2016-02-11 17:36:20 +0530

    added tests for edit customer controller and add customer  controller

---
+++ test/spec/controllers/testEditCustomerController.js
@@ -0,0 +1,111 @@
+
+(function () {'use strict';
+
+    describe('add customer controller Test', function () {
+        //mock object
+        var customers = {
+            'records': [
+                {
+                    'Name': 'Word Bell',
+                    'Location': 'Dellas,Texas',
+                    'orders': 1,
+                    'image' :'/images/User.jpg'
+                },
+                {
+                    "Name": "Candy sharon",
+                    "Location": "Dellas,Texas",
+                    "orders": 9,
+                    "image" :"/images/User.jpg"
+                }]};
+
+        //local variables
+        var $httpBackend;
+        var $http;
+        var service;
+        var masterDataService;
+        var scope;
+        var editCustomerCtrlScope;
+        var editCustomerCtrl;
+        var editCustomerDataService;
+        //setup
+        beforeEach(module('customerpageApp'));
+        //angular - mocks creation
+        beforeEach(inject(function ($rootScope,GetJSONDataService,MasterData,_$http_, _$httpBackend_,$controller,GetEditCustomerDataService) {
+            service = GetJSONDataService;
+            masterDataService = MasterData;
+            editCustomerDataService = GetEditCustomerDataService;
+            $http = _$http_;
+            $httpBackend = _$httpBackend_;
+            scope = $rootScope;
+            editCustomerCtrlScope = $rootScope.$new();
+            editCustomerCtrl = $controller('editCustomController', {
+                $scope: editCustomerCtrlScope
+
+            });
+
+        }));
+        beforeEach(function () {
+            $httpBackend.
+                whenGET('/customers/').
+                respond(function () { return [200, customers]; });
+
+        });
+        //tearDown
+        afterEach(function () {
+            $httpBackend.verifyNoOutstandingExpectation();
+            $httpBackend.verifyNoOutstandingRequest();
+        });
+
+        xit('Check  whether the mock is set properly for edit customer controller tests', function () {
+            var data;
+
+            $http.
+                get('/customers/').
+                then(function (response) {
+                    data = response.data;
+                });
+            $httpBackend.flush();
+        });
+
+
+        it('testing edit Customer Controller initialization',function(){
+
+            expect(scope.show).toBe(false);
+
+        });
+
+
+
+        it('testing saveEdited Customer function of edit Controller',function(){
+            editCustomerDataService.EditCustomerIndex = 0;
+            editCustomerDataService.EditCustomerData = {
+                'Name': 'Rajkumar',
+                'Location': 'Dellas,Texas',
+                'orders': 1,
+                'image': '/images/User.jpg'
+            };
+            service.getCustomerRecords(function(data){
+
+                var customerData = data.records;
+                masterDataService.layoutData = customerData;
+
+                editCustomerCtrlScope.SaveEditedCustomer();
+                expect(masterDataService.layoutData[0].Name).toBe('Rajkumar');
+
+            });
+
+            $httpBackend.flush();
+        });
+
+
+
+
+
+
+
+
+
+
+    });
+
+}());
---
+++ test/spec/controllers/testaddCustomerControllers.js
@@ -0,0 +1,117 @@
+
+(function () {'use strict';
+
+    describe('add customer controller Test', function () {
+        //mock object
+        var customers = {
+            'records': [
+                {
+                    'Name': 'Word Bell',
+                    'Location': 'Dellas,Texas',
+                    'orders': 1,
+                    'image' :'/images/User.jpg'
+                },
+                {
+                    "Name": "Candy sharon",
+                    "Location": "Dellas,Texas",
+                    "orders": 9,
+                    "image" :"/images/User.jpg"
+                }]};
+
+        //local variables
+        var $httpBackend;
+        var $http;
+        var service;
+        var masterDataService;
+        var scope;
+        var addCustomerCtrlScope;
+        var addCustomerCtrl;
+        //setup
+        beforeEach(module('customerpageApp'));
+        //angular - mocks creation
+        beforeEach(inject(function ($rootScope,GetJSONDataService,MasterData,_$http_, _$httpBackend_,$controller) {
+            service = GetJSONDataService;
+            masterDataService = MasterData;
+            $http = _$http_;
+            $httpBackend = _$httpBackend_;
+            scope = $rootScope;
+            addCustomerCtrlScope = $rootScope.$new();
+            addCustomerCtrl = $controller('addCustomController', {
+                $scope: addCustomerCtrlScope
+
+            });
+
+        }));
+        beforeEach(function () {
+            $httpBackend.
+                whenGET('/customers/').
+                respond(function () { return [200, customers]; });
+
+        });
+        //tearDown
+        afterEach(function () {
+            $httpBackend.verifyNoOutstandingExpectation();
+            $httpBackend.verifyNoOutstandingRequest();
+        });
+
+        xit('Check  whether the mock is set properly for add customer controller tests', function () {
+            var data;
+
+            $http.
+                get('/customers/').
+                then(function (response) {
+                    data = response.data;
+                });
+            $httpBackend.flush();
+        });
+
+
+           it('testing addCustomer Controller initialization',function(){
+
+            expect(scope.show).toBe(false);
+
+
+
+
+
+           // $httpBackend.flush();
+
+
+
+        });
+
+
+
+        it('testing add function for addCustomer Controller',function(){
+            addCustomerCtrlScope.customer = {
+                'Name': 'Word Bell',
+                'Location': 'Dellas,Texas',
+                'orders': 1,
+                'image': '/images/User.jpg'
+            };
+            service.getCustomerRecords(function(data){
+
+                    var customerData = data.records;
+                masterDataService.layoutData = customerData;
+
+                addCustomerCtrlScope.addCustomer();
+
+                expect(masterDataService.layoutData.length).toBe(3);
+
+            });
+
+            $httpBackend.flush();
+        });
+
+
+
+
+
+
+
+
+
+
+    });
+
+}());
